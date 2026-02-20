957ed8590eb9d6b84295378d5c0b47873343f9a2
ralphreevencarandang <rvncrndng@gmail.com>
2026-02-20 21:32:18 +0800

    feat: dockerized mern app using docker compose and watch

---
+++ mern-docker/frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Create from "./pages/Create";
+
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <nav className="navbar">
+        <Link to="/" className="navbar_title">
+          <h1>Anime</h1>
+        </Link>
+        <div className="navbar_links">
+          <Link to="/" className="navbar_link">
+            Home
+          </Link>
+          <Link to="/create" className="navbar_link">
+            Share
+          </Link>
+          <Link to="/create" className="navbar_link">
+            Popular
+          </Link>
+          <Link to="/create" className="navbar_link">
+            Tarubis
+          </Link>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
